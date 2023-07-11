d30747da3f7fc9a6362ce969aed73f54988f33ea
Julka Czarnasiak <czarna13500@gmail.com>
2023-07-11 18:46:25 +0200

    Add changes regarding review

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,14 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         String name1 = "Mleko";
-        String producent1 = "Mlekowita";
+        String producer1 = "Mlekowita";
         double price1 = 2.5;
         String name2 = "Czekolada";
-        String producent2 = "Wedel";
+        String producer2 = "Wedel";
         double price2 = 2.19;
         String currency = "zł";
 
-        System.out.printf("%s %s %f%s\n", name1, producent1, price1, currency);
-        System.out.printf("%s %s %f%s\n", name2, producent2, price2, currency);
+        System.out.printf("%s %s %f%s\n", name1, producer1, price1, currency);
+        System.out.printf("%s %s %f%s\n", name2, producer2, price2, currency);
     }
 }
